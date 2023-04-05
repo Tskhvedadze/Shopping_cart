ca53177dde5862f9bd127dea65db949398c8f820
Tskhvedadze <giorgicxvedadze4@gmail.com>
2023-04-05 20:21:44 +0400

    modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,14 @@
-import { lazy, Suspense } from 'react'
-import { Routes, Route } from 'react-router-dom'
-
 import { Container } from 'react-bootstrap'
 
-import { Navbar } from './components/Navbar'
-
-import { ShoppingCartProvider } from './context/ShoppingCartContext'
-
-const Home = lazy(() => import('./pages/Home'))
-const About = lazy(() => import('./pages/About'))
-const Store = lazy(() => import('./pages/Store'))
+import { Header, Body, Footer } from './components'
 
 function App() {
     return (
-        <ShoppingCartProvider>
-            <Navbar />
-            <Container className='mb-4'>
-                <Suspense fallback={<div>Loading...</div>}>
-                    <Routes>
-                        <Route index element={<Home />} />
-                        <Route path='/about' element={<About />} />
-                        <Route path='/store' element={<Store />} />
-                    </Routes>
-                </Suspense>
-            </Container>
-        </ShoppingCartProvider>
+        <Container>
+            <Header />
+            <Body />
+            <Footer />
+        </Container>
     )
 }
 
---
+++ src/components/Body.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+type Props = {}
+
+export const Body = (props: Props) => {
+    return <div>Body</div>
+}
